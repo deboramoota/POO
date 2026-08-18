301c34989fadfdfdf20a98d1b5f6ace3977656d4
Débora <debora.ribeiro63@aluno.ifce.edu.br>
2026-08-18 09:28:25 -0300

    Add files via upload

---
+++ src/br/edu/principal/Principal02.java
@@ -0,0 +1,56 @@
+
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class Principal02 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+
+        // Leitura dos dados
+		
+        System.out.print("Informe o Nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Informe a Rua: ");
+        String rua = scanner.nextLine();
+
+        System.out.print("Informe o Número: ");
+        String numero = scanner.nextLine();
+
+        System.out.print("Informe o Bairro: ");
+        String bairro = scanner.nextLine();
+
+        System.out.print("Informe o Complemento: ");
+        String complemento = scanner.nextLine();
+
+        System.out.print("Informe o CEP: ");
+        String cep = scanner.nextLine();
+
+        System.out.print("Informe a Cidade: ");
+        String cidade = scanner.nextLine();
+
+        System.out.print("Informe a UF: ");
+        String uf = scanner.nextLine();
+
+        System.out.print("Informe o CPF: ");
+        String cpf = scanner.nextLine();
+
+        System.out.print("Informe a Data de Nascimento: ): ");
+        String dataNascimento = scanner.nextLine();
+
+        System.out.print("Informe a Idade: ");
+        int idade = scanner.nextInt();
+
+        // Impressão no formato solicitado
+        
+        System.out.println("Nome : " + nome);
+        System.out.println("Endereço : " + rua + ", " + numero + ", " + bairro + ", " + complemento + ", " + cep + ", " + cidade + " - " + uf);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Data de Nascimeto: " + dataNascimento);
+        System.out.println("Idade: " + idade);
+
+    }
+}
